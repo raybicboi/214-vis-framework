75da8e706271d0b9df5af256c1cb8025a6c070f5
Dolev Artzi <dolevartzi@dolevs-mbp.myfiosgateway.com>
2021-11-19 11:17:25 -0500

    read-in-data-and-store-in-countries-API-plugin

--- src/main/java/data/ApiPlugin.java
+++ src/main/java/data/ApiPlugin.java
@@ -1,4 +1,145 @@
 package data;
 
-public class ApiPlugin {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.DoubleNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import country.Country;
+import framework.coreData.DataPlugin;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class ApiPlugin implements DataPlugin {
+
+    private Set<Country> countries;
+
+    /**
+     * Current API key to interact with this API:
+     *  https://api-ninjas.com/api/country
+     */
+    private static final String apiKey = "hU1pYu1g4/nxVl8vV97lsg==" +
+                                            "34WyNCC3ildfEQHe";
+    /**
+     * Query URL to extract all data
+     */
+    private static final String apiUrl = "https://api.api-ninjas.com/v1/" +
+                                            "country?min_gdp=1";
+
+    private static final String pluginName = "API Country Data Plugin";
+
+    /**
+     * Return the name of this plugin.
+     * @return {@link String}
+     */
+    public String getPluginName() {
+        return pluginName;
+    }
+
+    /**
+     * Returns the Json node that was returned as a response from the API
+     * when querying. This contains all the information for extraction,
+     * but can be null if there was an error.
+     * @param apiUrl
+     * @return {@link JsonNode} with data to be extracted.
+     */
+    private static JsonNode getAllData(String apiUrl) {
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection connection = (HttpURLConnection)
+                                                url.openConnection();
+            connection.setRequestProperty("X-Api-Key", apiKey);
+            InputStream responseStream = connection.getInputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readTree(responseStream);
+
+
+        } catch (Exception e) {
+            //TODO: figure out error handling
+            System.out.println("dang, we had an error. What should we do here?");
+        }
+
+        return null;
+    }
+
+    //TODO: figure out extracting partial data
+
+    /**
+     * Builds a set of all the countries from the given jsonNode.
+     * @param jsonNode
+     * @return a set of all the countries extracted from the JsonNode.
+     */
+    private static Set<Country> buildCountries(JsonNode jsonNode) {
+        Set<Country> res = new HashSet<>();
+        if (jsonNode == null || !jsonNode.isArray()) {
+            return res;
+        }
+
+        ArrayNode arrayNode = (ArrayNode) jsonNode;
+        int num_countries = arrayNode.size();
+
+        //builds each country one by one
+        for (int i = 0; i < num_countries; i++) {
+            Map<String, Double> countryDataSet = new HashMap<>();
+            ObjectNode col = (ObjectNode) arrayNode.get(i);
+            Iterator<Map.Entry<String, JsonNode>> fields = col.fields();
+
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> next = fields.next();
+                JsonNode val = next.getValue();
+                String key = next.getKey();
+                //only add numerical data to the data set
+                if (val instanceof DoubleNode) {
+                    countryDataSet.put(key,val.doubleValue());
+                }
+            }
+
+            String n = "name";
+            String r = "region";
+            String e = "";
+            //Sets empty string as default vals for name/region if absent
+            Country c = new Country(col.get(n).asText(e),col.get(r).asText(e));
+            c.setData(countryDataSet);
+            res.add(c);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        //extractDataStaticForTesting();
+    }
+
+    @Override
+    public void importData(String url) {
+        JsonNode node = getAllData(apiUrl);
+        countries = buildCountries(node);
+    }
+
+    @Override
+    public void getOptions(int[] options) {
+
+    }
+
+   // @Override -- Change back to normal..
+    /**
+     * Returns a set of all the {@link Country} that were acquired from the
+     * API and built to include all numerical data.
+     * @return {@link Set} of the countries.
+     */
+    public static Set<Country> extractDataStaticForTesting() {
+        JsonNode node = getAllData(apiUrl);
+        Set<Country> res = buildCountries(node);
+        //System.out.println(res);
+        return res;
+    }
+
+    @Override
+    public Set<Country> extractData() {
+        return countries;
+    }
+
 }
---
+++ src/test/java/data/ApiPluginTest.java
@@ -0,0 +1,75 @@
+package data;
+
+import country.Country;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class ApiPluginTest {
+    private static final int NUM_DATA_POINTS = 231;
+    Country japan;
+    ApiPlugin a;
+
+    @Before
+    public void setUp() {
+        a = new ApiPlugin();
+        japan = a.extractData().stream().filter
+                        (x -> x.getName().equals("Japan"))
+                .collect(Collectors.toList()).get(0);
+    }
+
+    /**
+     * Test that the correct amount of countries get extracted from the data.
+     */
+    @Test
+    public void testExtractDataSize() {
+        Set<Country> countries = a.extractData();
+        System.out.println(countries);
+        assertEquals(NUM_DATA_POINTS, countries.size());
+    }
+
+    /**
+     * Tests that the countries were made, choosing Japan arbitrarily
+     * as the country to check for existence.
+     */
+    @Test
+    public void testExtractDataNotNullCountry() {
+        assertNotNull(japan);
+    }
+
+    /**
+     * Checks that there are some data fields stored within the country after
+     * extracting data through the API.
+     */
+    @Test
+    public void testExtractDataFieldsAcquired() {
+        assertTrue(japan.dataPointCount() > 10);
+    }
+
+    @Test
+    public void testCorrectRegion() {
+        assertEquals("Eastern Asia", japan.getRegion());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
